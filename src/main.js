22fb73ccda18e5fff4fe7e6a30bc82057a6262a8
unknown <sbuteryd@qq.com>
2021-01-21 10:51:26 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import router from './router'
+
+// 全局css
+import './styles/index.css'
+// vant css
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// rem
+import 'amfe-flexible'
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/login/index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>russel</h1>
+        <div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+div {
+    width: 300px;
+    height: 300px;
+    background: pink;
+}
+</style>
